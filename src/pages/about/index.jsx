3bd17fa1f3a44a393ca7d351d61cc4bb646fa45d
hualalalalalala <490291490@qq.com>
2020-11-05 15:33:39 +0800

    添加关于我们页面

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { View} from '@tarojs/components'
+
+import './index.scss'
+
+export default class Index extends Component {
+
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='about'>
+        哈哈哈
+      </View>
+    )
+  }
+}
